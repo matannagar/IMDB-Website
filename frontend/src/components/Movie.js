89d1c52b47042ffeea1eb3d6ccc5b54ed4bf9e34
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-14 16:03:48 +0300

    fixed no image

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -2,9 +2,14 @@ import React from 'react'
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 function Movie({ title, poster_path, overview, vote_average }) {
+    const noImg = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
     return (
         <div className="movie">
-            <img src={IMG_API + poster_path} alt={title} />
+            <img src={poster_path ?
+                IMG_API + poster_path
+                : `${noImg}`
+            }
+                alt={title} />
             <div className="movie-info">
                 <h3>{title}</h3>
                 <span>{vote_average}</span>
