622666e2907338ed980068852f336bdf8b37e6ab
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-14 15:46:26 +0300

    search function is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import './index.css'
 import { Movie } from "./components";
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=84c35731a5ee918f014970082a0888b1&query="
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 export default function App(props) {
   const [movies, setMovies] = useState([]);
+  const [searchM, setSearchM] = useState('');
 
   useEffect(() => {
     fetch(FEATURED_API)
@@ -17,11 +18,35 @@ export default function App(props) {
       });
   }, [])
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(SEARCH_API + searchM)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+
+
+  };
+
+  const handleOnChange = (e) => {
+    setSearchM(e.target.value);
+  }
   return (
     <>
       <header>
-        <input className="search" type="search" placeholder="search..." />
+        <form onSubmit={handleOnSubmit}>
+          <input className="search"
+            type="search"
+            value={searchM}
+            onChange={handleOnChange}
+            placeholder="search..." />
+
+        </form>
       </header>
+
       <div className="movie-container">
 
         {movies.length > 0 &&
